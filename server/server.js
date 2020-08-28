33fb14bd56a8ff91a124720ad5dd8776bfe87f79
kidgray <micah.allauca@gmail.com>
2020-08-28 13:57:00 -0400

    Used Sequelize-CLI to create models and migrations

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const { GraphQLServer } = require('graphql-yoga');
 const { ApolloServer } = require('apollo-server');
 
+// Import instance of the sequelize Database
+const { sequelize } = require('./models/index');
+
 // GraphQL & Apollo Server utils
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers/index');
@@ -9,4 +12,14 @@ const resolvers = require('./graphql/resolvers/index');
 const server = new GraphQLServer({ typeDefs, resolvers });
 
 // Start the GraphQL Server
-server.start(({ port }) => console.log(`Server listening at http://localhost:${port}`));
+server.start(({ 
+    port 
+}) => console.log(`Server listening at http://localhost:${port}`))
+.then(() => {
+    // authenticate() returns a Promise; we return that from this 
+    // then() block so as to avoid excessive nesting. The next then()
+    // will handle this Promise
+    return sequelize.authenticate();
+})
+.then(() => console.log(`Database successfully authenticated and connected!`))
+.catch((err) => console.log(`Error: ${err}`));      // If the database failed to connect for whatever reason
