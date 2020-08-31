73b483110a39e627bd25560d90ab9ef7ae622ed9
kidgray <micah.allauca@gmail.com>
2020-08-30 21:47:04 -0400

    Finished writing the Registration page

--- client/src/pages/register/register-page.component.jsx
+++ client/src/pages/register/register-page.component.jsx
@@ -2,13 +2,19 @@ import React, { useState } from 'react';
 import { Container, Row, Col, Button, Form } from 'react-bootstrap';
 
 const RegisterPage = () => {
-    // 
+    // State Hook for the registration form fields
+    const [ registrationInfo, setRegistrationInfo ] = useState({
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+    });
 
     const handleSubmit = (event) => {
         // Prevent page refresh on form submission
         event.preventDefault();
 
-
+        console.log(registrationInfo);
     };
 
     return (
@@ -24,22 +30,38 @@ const RegisterPage = () => {
                     <Form onSubmit={handleSubmit}>
                         <Form.Group controlId='registerFormUsername'>
                             <Form.Label> Username </Form.Label>
-                            <Form.Control type="text" />
+                            <Form.Control 
+                                type="text" 
+                                value={registrationInfo.username}
+                                onChange={(event) => setRegistrationInfo({ ...registrationInfo, username: event.target.value })}
+                            />
                         </Form.Group>
 
                         <Form.Group controlId="registerFormEmail">
                             <Form.Label> Email Address </Form.Label>
-                            <Form.Control type="email" />
+                            <Form.Control 
+                                type="email" 
+                                value={registrationInfo.email}
+                                onChange={(event) => setRegistrationInfo({ ...registrationInfo, email: event.target.value })}
+                            />
                         </Form.Group>
 
                         <Form.Group controlId="registerFormPassword">
                             <Form.Label> Password </Form.Label>
-                            <Form.Control type="password" />
+                            <Form.Control 
+                                type="password" 
+                                value={registrationInfo.password}
+                                onChange={(event) => setRegistrationInfo({ ...registrationInfo, password: event.target.value })}
+                            />
                         </Form.Group>
 
                         <Form.Group controlId="registerFormConfirmPassword">
                             <Form.Label> Confirm Password </Form.Label>
-                            <Form.Control type="password" />
+                            <Form.Control 
+                                type="password" 
+                                value={registrationInfo.confirmPassword}
+                                onChange={(event) => setRegistrationInfo({ ...registrationInfo, confirmPassword: event.target.value })}
+                            />
                         </Form.Group>
 
                         <Button variant="primary" type="submit">
