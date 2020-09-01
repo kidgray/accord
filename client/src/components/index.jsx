e95554a96ceca61d7d5bb482da955f26cea6b7e1
kidgray <micah.allauca@gmail.com>
2020-08-31 21:46:19 -0400

    Wrote client-side Register mutation and connected Registration Page to back-end. Also some styling.

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { createHttpLink } from 'apollo-link-http';
 import { Container, Row, Col, Form } from 'react-bootstrap';
 
 // COMPONENTS & PAGES
 import RegisterPage from '../pages/register/register-page.component.jsx';
 
+// HTTP Link for use with the Apollo Client
+const httpLink = createHttpLink({
+    uri: 'http://localhost:4000'
+});
+
+// Create instance of Apollo Client
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+});
+
 const ApolloChatApp = () => {
     return (
-        <Container>
+        <ApolloProvider client={client}>
             <RegisterPage />
-        </Container>
+        </ApolloProvider>
     );
 };
 
--- client/src/pages/register/register-page.component.jsx
+++ client/src/pages/register/register-page.component.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Container, Row, Col, Button, Form } from 'react-bootstrap';
+import { Container, Row, Col, Button, Form, FormControl } from 'react-bootstrap';
+import { gql, useMutation } from '@apollo/client';
 
 const RegisterPage = () => {
     // State Hook for the registration form fields
@@ -10,11 +11,48 @@ const RegisterPage = () => {
         confirmPassword: ""
     });
 
+    // State Hook for errors that may occur during registration
+    const [ errors, setErrors ] = useState({});
+
+    // GraphQL MUTATIONS
+    const REGISTER_USER = gql`
+        mutation register(
+            $username: String!
+            $password: String!
+            $confirmPassword: String!
+            $email: String!
+        ) {
+            register(userInfo: {
+                username: $username
+                password: $password
+                confirmPassword: $confirmPassword
+                email: $email
+            }) {
+                username
+                email
+                createdAt
+            }
+        }
+    `;
+
+    // useMutation hook returns the mutation execution function and object with mutation state variables
+    const [ registerUser, { loading }] = useMutation(REGISTER_USER, {
+        update: (_, result) => {
+            console.log(result);
+        },
+        onError: (err) => {
+            // Populate the errors state variable
+            setErrors(err.graphQLErrors[0].extensions.errors);
+        },
+        variables: registrationInfo
+    });
+
     const handleSubmit = (event) => {
         // Prevent page refresh on form submission
         event.preventDefault();
 
-        console.log(registrationInfo);
+        // Pass the registrationInfo to the registerUser mutation
+        registerUser();
     };
 
     return (
@@ -29,43 +67,75 @@ const RegisterPage = () => {
                 <Col sm={8} md={6} lg={4}>
                     <Form onSubmit={handleSubmit}>
                         <Form.Group controlId='registerFormUsername'>
-                            <Form.Label> Username </Form.Label>
+                            <Form.Label className={errors.username && 'text-danger'}> 
+                                Username 
+                            </Form.Label>
+
                             <Form.Control 
                                 type="text" 
                                 value={registrationInfo.username}
                                 onChange={(event) => setRegistrationInfo({ ...registrationInfo, username: event.target.value })}
+                                isInvalid={errors.username}
                             />
+
+                            <FormControl.Feedback type="invalid">
+                                { errors.username }
+                            </FormControl.Feedback>
                         </Form.Group>
 
                         <Form.Group controlId="registerFormEmail">
-                            <Form.Label> Email Address </Form.Label>
+                            <Form.Label className={errors.email && 'text-danger'}> 
+                                Email Address 
+                            </Form.Label>
+
                             <Form.Control 
                                 type="email" 
                                 value={registrationInfo.email}
                                 onChange={(event) => setRegistrationInfo({ ...registrationInfo, email: event.target.value })}
+                                isInvalid={errors.email}
                             />
+
+                            <FormControl.Feedback type="invalid">
+                                { errors.email }
+                            </FormControl.Feedback>
                         </Form.Group>
 
                         <Form.Group controlId="registerFormPassword">
-                            <Form.Label> Password </Form.Label>
+                            <Form.Label className={errors.password && 'text-danger'}> 
+                                Password 
+                            </Form.Label>
+
                             <Form.Control 
                                 type="password" 
                                 value={registrationInfo.password}
                                 onChange={(event) => setRegistrationInfo({ ...registrationInfo, password: event.target.value })}
+                                isInvalid={errors.password}
                             />
+
+                            <FormControl.Feedback type="invalid">
+                                { errors.password }
+                            </FormControl.Feedback>
                         </Form.Group>
 
                         <Form.Group controlId="registerFormConfirmPassword">
-                            <Form.Label> Confirm Password </Form.Label>
+                            <Form.Label className={errors.password && 'text-danger'}> 
+                                Confirm Password 
+                            </Form.Label>
+
                             <Form.Control 
                                 type="password" 
                                 value={registrationInfo.confirmPassword}
                                 onChange={(event) => setRegistrationInfo({ ...registrationInfo, confirmPassword: event.target.value })}
+                                isInvalid={errors.confirmPassword}
                             />
+
+                            <FormControl.Feedback type="invalid">
+                                { errors.confirmPassword }
+                            </FormControl.Feedback>
                         </Form.Group>
 
-                        <Button variant="primary" type="submit">
-                            Register!
+                        <Button variant="primary" type="submit" disabled={loading}>
+                            { loading ? 'Loading...' : 'Register!' }
                         </Button>
                     </Form>
                 </Col>
