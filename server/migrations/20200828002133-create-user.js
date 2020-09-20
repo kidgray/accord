22e9024200ac9f128d3a9f14bad7d63727c2c583
kidgray <micah.allauca@gmail.com>
2020-09-20 00:52:18 -0400

    Project ready for deployment.

--- server/migrations/20200828002133-create-user.js
+++ server/migrations/20200828002133-create-user.js
@@ -4,7 +4,7 @@ const { DataTypes } = require("sequelize");
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('users', {
       id: {
         allowNull: false,
         autoIncrement: true,
